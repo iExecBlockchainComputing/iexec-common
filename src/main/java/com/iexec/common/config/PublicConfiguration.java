d596f21a1fdb163da3ca4c6bd8b4709cb0dad202
Ugo Plouviez <up@iex.ec>
2018-10-02 16:46:51 +0200

    Add public configuration

---
+++ src/main/java/com/iexec/common/config/PublicConfiguration.java
@@ -0,0 +1,16 @@
+package com.iexec.common.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PublicConfiguration {
+
+    private String blockchainAddress;
+}
+
