4d0839fc90d91323d9f02dcab544ad8501d6f09a
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2018-11-16 12:05:35 +0100

    Updated replicate status for blockchain

--- src/main/java/com/iexec/common/replicate/ReplicateStatus.java
+++ src/main/java/com/iexec/common/replicate/ReplicateStatus.java
@@ -2,5 +2,10 @@ package com.iexec.common.replicate;
 
 public enum ReplicateStatus {
 
-    CREATED, RUNNING, COMPUTED, UPLOAD_RESULT_REQUEST_FAILED, UPLOADING_RESULT, RESULT_UPLOADED, WORKER_LOST, ERROR
+    CREATED, RUNNING, COMPUTED, CONTRIBUTED, REVEALED, UPLOAD_RESULT_REQUEST_FAILED, UPLOADING_RESULT, RESULT_UPLOADED, WORKER_LOST, ERROR;
+
+    public static boolean isBlockchainStatus(ReplicateStatus status) {
+        return status.equals(ReplicateStatus.CONTRIBUTED)
+                || status.equals(ReplicateStatus.REVEALED);
+    }
 }
