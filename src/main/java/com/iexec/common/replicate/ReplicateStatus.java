da37c22abeddc12b7b61d03b2a5c17f908795bdd
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2018-11-28 16:36:35 +0100

    New replicates statuses

--- src/main/java/com/iexec/common/replicate/ReplicateStatus.java
+++ src/main/java/com/iexec/common/replicate/ReplicateStatus.java
@@ -1,25 +1,26 @@
 package com.iexec.common.replicate;
 
-import com.iexec.common.chain.ChainContributionStatus;
-
 public enum ReplicateStatus {
 
-    CREATED, RUNNING, COMPUTED, CONTRIBUTED, REVEALED, UPLOAD_RESULT_REQUEST_FAILED, UPLOADING_RESULT,
-    RESULT_UPLOADED, COMPLETED, WORKER_LOST, ERROR;
-
-    public static boolean isBlockchainStatus(ReplicateStatus status) {
-        return status.equals(ReplicateStatus.CONTRIBUTED)
-                || status.equals(ReplicateStatus.REVEALED);
-    }
-
-    public static ChainContributionStatus getChainStatus(ReplicateStatus replicateStatus) {
-        switch (replicateStatus){
-            case CONTRIBUTED:
-                return ChainContributionStatus.CONTRIBUTED;
-            case REVEALED:
-                return ChainContributionStatus.REVEALED;
-            default:
-                return null;
-        }
-    }
+    CREATED,
+    RUNNING,
+    APP_DOWNLOADING,
+    APP_DOWNLOADED,
+    APP_DOWNLOAD_FAILED,
+    COMPUTING,
+    COMPUTED,
+    COMPUTE_FAILED,
+    CONTRIBUTING,
+    CONTRIBUTED,
+    CONTRIBUTE_FAILED,
+    REVEALING,
+    REVEALED,
+    REVEAL_FAILED,
+    UPLOAD_RESULT_REQUEST_FAILED,
+    RESULT_UPLOADING,
+    RESULT_UPLOADED,
+    RESULT_UPLOAD_FAILED,
+    COMPLETED,
+    WORKER_LOST,
+    ERROR;
 }
