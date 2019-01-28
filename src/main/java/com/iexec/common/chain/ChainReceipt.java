5e35735f1f1b68b1f9f03504607c7f5f110fa407
Zied <guesmy.zied@gmail.com>
2019-01-28 15:27:20 +0100

    build chain receipt

--- src/main/java/com/iexec/common/chain/ChainReceipt.java
+++ src/main/java/com/iexec/common/chain/ChainReceipt.java
@@ -6,9 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ChainReceipt {
     private long blockNumber;
     private String txHash;
