8bde6f7e0db65111fe2caa24a6054757edb2445b
Ugo Plouviez <up@iex.ec>
2019-02-20 10:31:03 +0100

    Add TEE tag

--- src/main/java/com/iexec/common/tee/TeeUtils.java
+++ src/main/java/com/iexec/common/tee/TeeUtils.java
@@ -5,14 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TeeUtils {
 
+    private static final String TEE_TAG = "0x0000000000000000000000000000000000000000000000000000000000000001";
+
     private TeeUtils() {
         throw new UnsupportedOperationException();
     }
 
-
     //TODO : xor instead of equals
     public static boolean isTrustedExecutionTag(String tag){
-        return tag != null && tag.equals("0x01");
+        return tag != null && tag.equals(TEE_TAG);
     }
 
 }
