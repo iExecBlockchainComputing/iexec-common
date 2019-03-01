636de942fe21588582a9fa1e81ab1351d5152f47
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-03-01 17:43:24 +0100

    Move web3 and hub to common

--- src/main/java/com/iexec/common/chain/Web3jAbstractService.java
+++ src/main/java/com/iexec/common/chain/Web3jAbstractService.java
@@ -5,17 +5,49 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.ContractGasProvider;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Optional;
+
+import static com.iexec.common.chain.ChainUtils.weiToEth;
+import static com.iexec.common.contract.generated.IexecHubABILegacy.*;
 
 @Slf4j
 public abstract class Web3jAbstractService {
 
-    protected final Web3j web3j;
+    private final static long GAS_LIMIT_CAP = 500000;
+    private final Web3j web3j;
+    private final float gasPriceMultiplier;
+    private final long gasPriceCap;
 
-    public Web3jAbstractService(String chainAddress) {
-        this.web3j = ChainUtils.getWeb3j(chainAddress);
+    public Web3jAbstractService(String chainNodeAddress,
+                                float gasPriceMultiplier,
+                                long gasPriceCap) {
+        this.web3j = getWeb3j(chainNodeAddress);
+        this.gasPriceMultiplier = gasPriceMultiplier;
+        this.gasPriceCap = gasPriceCap;
+    }
+
+    public static BigInteger getMaxTxCost(long gasPriceCap) {
+        return BigInteger.valueOf(GAS_LIMIT_CAP * gasPriceCap);
+    }
+
+    private Web3j getWeb3j(String chainNodeAddress) {
+        Web3j web3j = Web3j.build(new HttpService(chainNodeAddress));
+        ExceptionInInitializerError exceptionInInitializerError = new ExceptionInInitializerError("Failed to connect to ethereum node " + chainNodeAddress);
+        try {
+            log.info("Connected to Ethereum node [node:{}]", web3j.web3ClientVersion().send().getWeb3ClientVersion());
+            if (web3j.web3ClientVersion().send().getWeb3ClientVersion() == null) {
+                throw exceptionInInitializerError;
+            }
+        } catch (IOException e) {
+            throw exceptionInInitializerError;
+        }
+        return web3j;
     }
 
     public Web3j getWeb3j() {
@@ -73,4 +105,129 @@ public abstract class Web3jAbstractService {
         }
         return maxWaitingTime;
     }
+
+    public boolean hasEnoughGas(String address) {
+        Optional<BigInteger> optionalBalance = getBalance(address);
+        if (!optionalBalance.isPresent()) {
+            return false;
+        }
+
+        BigInteger weiBalance = optionalBalance.get();
+        BigInteger estimateTxNb = weiBalance.divide(getMaxTxCost(gasPriceCap));
+        BigDecimal balanceToShow = weiToEth(weiBalance);
+
+        if (estimateTxNb.compareTo(BigInteger.ONE) < 0) {
+            log.error("ETH balance is empty, please refill gas now [balance:{}, estimateTxNb:{}]", balanceToShow, estimateTxNb);
+            return false;
+        } else if (estimateTxNb.compareTo(BigInteger.TEN) < 0) {
+            log.warn("ETH balance very low, should refill gas now [balance:{}, estimateTxNb:{}]", balanceToShow, estimateTxNb);
+        } else {
+            log.info("ETH balance is fine [balance:{}, estimateTxNb:{}]", balanceToShow, estimateTxNb);
+        }
+        return true;
+    }
+
+    public Optional<BigInteger> getBalance(String address) {
+        try {
+            return Optional.of(web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send().getBalance());
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<BigInteger> getNetworkGasPrice() {
+        try {
+            BigInteger gasPrice = web3j.ethGasPrice().send().getGasPrice();
+            return Optional.of(gasPrice);
+        } catch (IOException e) {
+            log.error("getNetworkGasPrice failed");
+            return Optional.empty();
+        }
+    }
+
+    public BigInteger getUserGasPrice(float gasPriceMultiplier, long gasPriceCap) {
+        Optional<BigInteger> networkGasPrice = getNetworkGasPrice();
+        if (!networkGasPrice.isPresent()) {
+            return BigInteger.valueOf(gasPriceCap);
+        }
+        long wishedGasPrice = (long) (networkGasPrice.get().floatValue() * gasPriceMultiplier);
+
+        return BigInteger.valueOf(Math.min(wishedGasPrice, gasPriceCap));
+    }
+
+    /*
+     * This is just a dummy stub for contract reader:
+     * gas price & gas limit is not required when querying (read) an eth node
+     *
+     */
+    public ContractGasProvider getReadingContractGasProvider() {
+        return new ContractGasProvider() {
+            @Override
+            public BigInteger getGasPrice(String contractFunc) {
+                return null;
+            }
+
+            @Override
+            public BigInteger getGasPrice() {
+                return null;
+            }
+
+            @Override
+            public BigInteger getGasLimit(String contractFunc) {
+                return null;
+            }
+
+            @Override
+            public BigInteger getGasLimit() {
+                return null;
+            }
+        };
+    }
+
+    public ContractGasProvider getWritingContractGasProvider() {
+        return new ContractGasProvider() {
+
+            @Override
+            public BigInteger getGasPrice(String s) {
+                return getUserGasPrice(gasPriceMultiplier, gasPriceCap);
+            }
+
+            @Override
+            public BigInteger getGasPrice() {
+                return getUserGasPrice(gasPriceMultiplier, gasPriceCap);
+            }
+
+            @Override
+            public BigInteger getGasLimit(String functionName) {
+                long gasLimit;
+                switch (functionName) {
+                    case FUNC_INITIALIZE:
+                        gasLimit = 300000;//seen 176340
+                        break;
+                    case FUNC_CONTRIBUTEABILEGACY:
+                        gasLimit = 500000;//seen 333541
+                        break;
+                    case FUNC_REVEAL:
+                        gasLimit = 100000;//seen 56333
+                        break;
+                    case FUNC_FINALIZE:
+                        gasLimit = 3000000;//seen 175369 (242641 in reopen case)
+                        break;
+                    case FUNC_REOPEN:
+                        gasLimit = 500000;//seen 43721
+                        break;
+                    default:
+                        gasLimit = GAS_LIMIT_CAP;
+                }
+                return BigInteger.valueOf(gasLimit);
+            }
+
+            @Override
+            public BigInteger getGasLimit() {
+                return BigInteger.valueOf(GAS_LIMIT_CAP);
+            }
+        };
+    }
+
+
 }
