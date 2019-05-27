21288939103b93ef9d817f3b38bd59eb572fad12
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-05-27 14:53:18 +0200

    Notification for recovery

--- src/main/java/com/iexec/common/disconnection/InterruptedReplicateModel.java
+++ src/main/java/com/iexec/common/disconnection/InterruptedReplicateModel.java
@@ -1,8 +1,8 @@
 package com.iexec.common.disconnection;
 
 import com.iexec.common.chain.ContributionAuthorization;
-import com.iexec.common.disconnection.RecoveryAction;
 
+import com.iexec.common.notification.TaskNotificationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,5 +16,5 @@ import lombok.NoArgsConstructor;
 public class InterruptedReplicateModel {
 
     ContributionAuthorization contributionAuthorization;
-    RecoveryAction recoveryAction;
+    TaskNotificationType taskNotificationType;
 }
