d5e61b8c0fa14e86711a3e97053bdb9dcd10a0a4
Ugo Plouviez <up@iex.ec>
2019-07-18 15:07:17 +0200

    Remove useless Nullable

--- src/main/java/com/iexec/common/chain/DealParams.java
+++ src/main/java/com/iexec/common/chain/DealParams.java
@@ -15,7 +15,5 @@ public class DealParams {
     // Note to dev: the naming of the variables here is important since it will be stored on-chain
     // that is why the names are not java compliant (use of underscore and no upper case)
     private String iexec_args;
-
-    @Nullable
     private List<String> iexec_input_files;
 }
