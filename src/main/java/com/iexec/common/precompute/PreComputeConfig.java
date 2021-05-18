23a18dc44631868fef27f8924323ddda294b3e6c
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2021-05-18 13:57:25 +0200

    Cleaned pre-compute config class

--- src/main/java/com/iexec/common/precompute/PreComputeConfig.java
+++ src/main/java/com/iexec/common/precompute/PreComputeConfig.java
@@ -1,10 +1,11 @@
 package com.iexec.common.precompute;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class PreComputeConfig {
 
@@ -12,18 +13,4 @@ public class PreComputeConfig {
     private String fingerprint;
     private String heapSize;
 
-    public PreComputeConfig(String image, String fingerprint, String heapSize) {
-        if (StringUtils.isEmpty(image)) {
-            throw new IllegalStateException("No pre-compute image is provided");
-        }
-        if (StringUtils.isEmpty(fingerprint)) {
-            throw new IllegalStateException("No pre-compute fingerprint is provided");
-        }
-        if (StringUtils.isEmpty(heapSize)) {
-            throw new IllegalStateException("No pre-compute heap size is provided");
-        }
-        this.image = image;
-        this.fingerprint = fingerprint;
-        this.heapSize = heapSize;
-    }
 }
