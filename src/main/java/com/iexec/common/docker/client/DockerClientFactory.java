ee0c2b5fd8fd688fd6989cc06ed9c70c95acb15c
Zied <26070035+zguesmi@users.noreply.github.com>
2021-06-14 09:09:02 +0200

    Add authenticated docker clients

--- src/main/java/com/iexec/common/docker/client/DockerClientFactory.java
+++ src/main/java/com/iexec/common/docker/client/DockerClientFactory.java
@@ -16,18 +16,71 @@
 
 package com.iexec.common.docker.client;
 
+import com.github.dockerjava.api.exception.DockerException;
+import com.github.dockerjava.api.model.AuthConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class DockerClientFactory {
 
-    private static DockerClientInstance defaultClient;
+    private static Map<String, DockerClientInstance> clientsMap = new HashMap<>();
 
+    /**
+     * Get an unauthenticated Docker client instance.
+     * 
+     * @return unauthenticated client
+     */
     public static synchronized DockerClientInstance getDockerClientInstance() {
-        if (defaultClient == null) {
-            defaultClient = new DockerClientInstance();
-        }
-        return defaultClient;
+        return createInstance("", "", "");
     }
 
-    public static synchronized DockerClientInstance getDockerClientInstance(String username, String password) {
-        return new DockerClientInstance(username, password);
+    /**
+     * Get a Docker client instance that is authenticated against the default docker
+     * registry {@link AuthConfig#DEFAULT_SERVER_ADDRESS}
+     * ({@code https://index.docker.io/v1/}) using the provided credentials.
+     * 
+     * @param username
+     * @param password
+     * @throws DockerException if the authentication fails
+     */
+    public static synchronized DockerClientInstance getDockerClientInstance(
+            String username, String password) throws DockerException {
+        return createInstance("", username, password);
+    }
+
+    /**
+     * Get a Docker client that is authenticated against a specific registry with the
+     * provided credentials.
+     * 
+     * @param registryUrl e.g. {@code https://index.docker.io/v1/, https://nexus.iex.ec,
+     *                          docker.io, nexus.iex.ec}
+     * @param username
+     * @param password
+     * @throws DockerException if the authentication fails
+     */
+    public static synchronized DockerClientInstance getDockerClientInstance(
+            String registryUrl, String username, String password) throws DockerException {
+        return createInstance(registryUrl, username, password);
+    }
+
+    /**
+     * Used at least in unit tests.
+     */
+    static void purgeClients() {
+        clientsMap.clear();
+    }
+
+    private static DockerClientInstance createInstance(
+            String registryUrl, String username, String password) {
+        String id = getClientIdentifier(registryUrl, username);
+        if (clientsMap.get(id) == null) {
+            clientsMap.put(id, new DockerClientInstance(registryUrl, username, password));
+        }
+        return clientsMap.get(id);
+    }
+
+    private static String getClientIdentifier(String registryUrl, String username) {
+        return registryUrl + username;
     }
 }
