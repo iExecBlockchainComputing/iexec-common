771ad396bf642bb6d732b4ac35888b24fc1bf74a
maxence <maxence.cornaton@iex.ec>
2021-11-15 15:51:37 +0100

    Wrap line

--- src/main/java/com/iexec/common/chain/IexecHubAbstractService.java
+++ src/main/java/com/iexec/common/chain/IexecHubAbstractService.java
@@ -140,7 +140,8 @@ public abstract class IexecHubAbstractService {
         this.iexecHubAddress = iexecHubAddress;
         this.nbBlocksToWaitPerRetry = nbBlocksToWaitPerRetry;
         if (blockTime == null || blockTime.toMillis() <= 0) {
-            log.warn("Blocktime value is incorrect, using default value [blockTime:{}, DEFAULT_BLOCK_TIME:{}]",
+            log.warn("Block time value is incorrect, using default value "
+                            + "[blockTime:{}, DEFAULT_BLOCK_TIME:{}]",
                     blockTime, DEFAULT_BLOCK_TIME);
             this.blockTime = Duration.ofMillis(DEFAULT_BLOCK_TIME);
         } else {
