8f9f7d799e02cb211344e2f7bccb53a3212cf64f
maxence <maxence.cornaton@iex.ec>
2021-12-23 17:29:06 +0100

    Add `TargetedLock`

---
+++ src/main/java/com/iexec/common/utils/TargetedLock.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.iexec.common.utils;
+
+import net.jodah.expiringmap.ExpiringMap;
+
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Provides a way to avoid parallel runs of an action.
+ * Locks are based on a key, so that two actions with the same key can't be run at the same time
+ * but two actions with different keys can be run at the same time.
+ * <br>
+ * To avoid memory-leaks, entries expire after a given time.
+ * Default is 1 hour, but it can be set at {@link TargetedLock} object creation.
+ *
+ * @param <K> Type of the key.
+ */
+public class TargetedLock<K> {
+    private final ConcurrentMap<K, Object> locks;
+
+    public TargetedLock() {
+        this(1, TimeUnit.HOURS);
+    }
+
+    public TargetedLock(long expirationDuration, TimeUnit unit) {
+        this.locks = ExpiringMap.builder()
+                .expiration(expirationDuration, unit)
+                .build();
+    }
+
+    public void runWithLock(K key, Runnable action) {
+        // `ConcurrentHashMap::computeIfAbsent` is atomic, so there can't be any race condition there.
+        synchronized (locks.computeIfAbsent(key, id -> new Object())) {
+            action.run();
+        }
+    }
+
+    public void acceptWithLock(K key, Consumer<K> action) {
+        // `ConcurrentHashMap::computeIfAbsent` is atomic, so there can't be any race condition there.
+        synchronized (locks.computeIfAbsent(key, id -> new Object())) {
+            action.accept(key);
+        }
+    }
+
+    public <R> R getWithLock(K key, Supplier<R> action) {
+        // `ConcurrentHashMap::computeIfAbsent` is atomic, so there can't be any race condition there.
+        synchronized (locks.computeIfAbsent(key, id -> new Object())) {
+            return action.get();
+        }
+    }
+
+    public <R> R applyWithLock(K key, Function<K, R> action) {
+        // `ConcurrentHashMap::computeIfAbsent` is atomic, so there can't be any race condition there.
+        synchronized (locks.computeIfAbsent(key, id -> new Object())) {
+            return action.apply(key);
+        }
+    }
+}
