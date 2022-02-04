b82e6045168e68f9b6fc273a6273d969cd0265c0
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-02-04 16:44:50 +0100

    Add FeignBuilder class with its test class

---
+++ src/main/java/com/iexec/common/utils/FeignBuilder.java
@@ -0,0 +1,58 @@
+package com.iexec.common.utils;
+
+import feign.Feign;
+import feign.Logger;
+import feign.RequestTemplate;
+import feign.Response;
+import feign.codec.Encoder;
+import feign.codec.StringDecoder;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import feign.slf4j.Slf4jLogger;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+/**
+ * Returns a Feign builder with shared configuration to create various Feign client instances.
+ * <p>
+ * The shared configuration is:
+ * <ul>
+ * <li>JacksonEncoder with overridden encode method to deal with strings
+ * <li>JacksonDecoder with overridden decode method to deal with strings
+ * <li>SLF4J logger enabled with FULL details. Logs are displayed by configuring the log level on a target interface logger.
+ * <li>application/json Content-Type header
+ * </ul>
+ */
+public class FeignBuilder {
+
+    private FeignBuilder() {};
+
+    public static Feign.Builder createBuilder() {
+        return Feign.builder()
+                .encoder(new JacksonEncoder() {
+                    @Override
+                    public void encode(Object object, Type bodyType, RequestTemplate template) {
+                        if (bodyType == String.class) {
+                            new Encoder.Default().encode(object, bodyType, template);
+                        } else {
+                            super.encode(object, bodyType, template);
+                        }
+                    }
+                })
+                .decoder(new JacksonDecoder() {
+                    @Override
+                    public Object decode(Response response, Type type) throws IOException {
+                        if (type == String.class) {
+                            return new StringDecoder().decode(response, type);
+                        } else {
+                            return super.decode(response, type);
+                        }
+                    }
+                })
+                .logger(new Slf4jLogger())
+                .logLevel(Logger.Level.FULL)
+                .requestInterceptor(template -> template.header("Content-Type", "application/json"));
+    }
+
+}
---
+++ src/test/java/com/iexec/common/utils/FeignBuilderTest.java
@@ -0,0 +1,116 @@
+package com.iexec.common.utils;
+
+import feign.RequestLine;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockserver.client.MockServerClient;
+import org.testcontainers.containers.MockServerContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+/**
+ * The purpose of those tests is to check the {@code JacksonEncoder#encode}
+ * and {@code JacksonDecoder#decode} methods are properly overridden.
+ */
+@Slf4j
+public class FeignBuilderTest {
+
+    @Container
+    private static final MockServerContainer mockServer = new MockServerContainer(DockerImageName.parse("mockserver/mockserver"))
+            .withExposedPorts(1080);
+
+    private FeignTestClient feignTestClient;
+
+    @BeforeEach
+    void preflight() {
+        log.info("preflight");
+        mockServer.start();
+        log.info("host {}", mockServer.getHost());
+        log.info("port {}", mockServer.getServerPort());
+        feignTestClient = FeignBuilder.createBuilder()
+                .target(FeignTestClient.class,
+                        "http://" + mockServer.getHost() + ":" + mockServer.getServerPort());
+    }
+
+    @Test
+    void testStringDecoder() {
+        new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
+                .when(request()
+                        .withMethod("GET")
+                        .withPath("/string"))
+                .respond(response()
+                        .withBody("stringValue"));
+        String value = feignTestClient.getStringValue();
+        Assertions.assertEquals("stringValue", value);
+    }
+
+    @Test
+    void testStringEncoder() {
+        new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/string"))
+                .respond(response()
+                        .withBody("")
+                        .withStatusCode(200));
+        Assertions.assertEquals("", feignTestClient.setStringValue("stringValue"));
+    }
+
+    @Test
+    void testJsonDecoder() {
+        new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
+                .when(request()
+                        .withMethod("GET")
+                        .withPath("/object"))
+                .respond(response()
+                        .withBody("{\"objectField\": \"objectValue\"}"));
+        TestObject testObject = feignTestClient.getTestObject();
+        Assertions.assertEquals("objectValue", testObject.getObjectField());
+    }
+
+    @Test
+    void testJsonEncoder() {
+        new MockServerClient(mockServer.getHost(), mockServer.getServerPort())
+                .when(request()
+                        .withMethod("POST")
+                        .withPath("/object"))
+                .respond(response()
+                        .withBody("")
+                        .withStatusCode(200));
+        TestObject testObject = new TestObject();
+        testObject.setObjectField("objectValue");
+        Assertions.assertEquals("", feignTestClient.setTestObject(testObject));
+    }
+
+    interface FeignTestClient {
+
+        @RequestLine("GET /string")
+        String getStringValue();
+
+        @RequestLine("POST /string")
+        String setStringValue(String value);
+
+        @RequestLine("GET /object")
+        TestObject getTestObject();
+
+        @RequestLine("POST /object")
+        String setTestObject(TestObject testObject);
+
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    static class TestObject {
+        private String objectField;
+    }
+
+}
