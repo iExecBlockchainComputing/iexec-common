603449e6acacdecf9835c588f6fd1984e71daa4a
maxence <maxence.cornaton@iex.ec>
2022-02-09 16:34:12 +0100

    Rename `SGX_DRIVER_PATH` into `LEGACY_SGX_DRIVER_PATH`

--- src/main/java/com/iexec/common/utils/SgxUtils.java
+++ src/main/java/com/iexec/common/utils/SgxUtils.java
@@ -2,5 +2,5 @@ package com.iexec.common.utils;
 
 public class SgxUtils {
     public static final String SGX_CGROUP_PERMISSIONS = "rwm";
-    public static final String SGX_DRIVER_PATH = "/sys/module/isgx/version";
+    public static final String LEGACY_SGX_DRIVER_PATH = "/sys/module/isgx/version";
 }
