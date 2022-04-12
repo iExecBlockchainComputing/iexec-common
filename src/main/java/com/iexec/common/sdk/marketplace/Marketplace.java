eed86f4c2828c72a4e181ba2e57f62b1d19bcc6a
maxence <maxence.cornaton@iex.ec>
2022-04-12 14:23:18 +0200

    Rename `getWorkerpoolOrder[s]` methods to be more uniform

--- src/main/java/com/iexec/common/sdk/marketplace/Marketplace.java
+++ src/main/java/com/iexec/common/sdk/marketplace/Marketplace.java
@@ -27,15 +27,31 @@ public interface Marketplace {
 
     public List<WorkerpoolMarketOrder> getWorkerpoolOrders();
 
-    public Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrder(String workerpoolAddress);
+    public Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrderByWorkerpoolAddress(String workerpoolAddress);
 
-    public List<WorkerpoolMarketOrder> getWorkerpoolOrders(String workerpoolAddress);
+    /**
+     * @deprecated Use {@link Marketplace#getOneWorkerpoolOrderByWorkerpoolAddress(String)} instead.
+     */
+    @Deprecated(forRemoval = true)
+    default Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrder(String workerpoolAddress) {
+        return getOneWorkerpoolOrderByWorkerpoolAddress(workerpoolAddress);
+    }
+
+    public List<WorkerpoolMarketOrder> getWorkerpoolOrdersByWorkerpoolAddress(String workerpoolAddress);
+
+    /**
+     * @deprecated Use {@link Marketplace#getWorkerpoolOrdersByWorkerpoolAddress(String)} instead.
+     */
+    @Deprecated(forRemoval = true)
+    default List<WorkerpoolMarketOrder> getWorkerpoolOrders(String workerpoolAddress) {
+        return getWorkerpoolOrdersByWorkerpoolAddress(workerpoolAddress);
+    }
 
     List<WorkerpoolMarketOrder> getWorkerpoolOrdersByTag(String tag);
 
-    Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrderByTag(String workerpoolAddress, String tag);
+    Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrderByWorkerpoolAddressAndTag(String workerpoolAddress, String tag);
 
-    List<WorkerpoolMarketOrder> getWorkerpoolOrdersByTag(String workerpoolAddress, String tag);
+    List<WorkerpoolMarketOrder> getWorkerpoolOrdersByWorkerpoolAddressAndTag(String workerpoolAddress, String tag);
 
     public Optional<AppMarketOrder> getOneAppOrder(String appAddress);
 
--- src/main/java/com/iexec/common/sdk/marketplace/MarketplaceClient.java
+++ src/main/java/com/iexec/common/sdk/marketplace/MarketplaceClient.java
@@ -64,14 +64,14 @@ public class MarketplaceClient implements Marketplace {
     }
 
     @Override
-    public Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrder(String workerpoolAddress) {
-        return getWorkerpoolOrders(workerpoolAddress)
+    public Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrderByWorkerpoolAddress(String workerpoolAddress) {
+        return getWorkerpoolOrdersByWorkerpoolAddress(workerpoolAddress)
                 .stream()
                 .findFirst();        
     }
 
     @Override
-    public List<WorkerpoolMarketOrder> getWorkerpoolOrders(String workerpoolAddress) {
+    public List<WorkerpoolMarketOrder> getWorkerpoolOrdersByWorkerpoolAddress(String workerpoolAddress) {
         MPQueryParams queryParams = MPQueryParams.builder()
                 .workerpool(workerpoolAddress)
                 .build();
@@ -88,14 +88,14 @@ public class MarketplaceClient implements Marketplace {
     }
 
     @Override
-    public Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrderByTag(String workerpoolAddress, String tag) {
-        return getWorkerpoolOrdersByTag(workerpoolAddress, tag)
+    public Optional<WorkerpoolMarketOrder> getOneWorkerpoolOrderByWorkerpoolAddressAndTag(String workerpoolAddress, String tag) {
+        return getWorkerpoolOrdersByWorkerpoolAddressAndTag(workerpoolAddress, tag)
                 .stream()
                 .findFirst();
     }
 
     @Override
-    public List<WorkerpoolMarketOrder> getWorkerpoolOrdersByTag(String workerpoolAddress, String tag) {
+    public List<WorkerpoolMarketOrder> getWorkerpoolOrdersByWorkerpoolAddressAndTag(String workerpoolAddress, String tag) {
         MPQueryParams queryParams = MPQueryParams.builder()
                 .workerpool(workerpoolAddress)
                 .minTag(tag)
