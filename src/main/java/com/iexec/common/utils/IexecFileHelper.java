3119db4a7b87f04aa20128420a8fdae601b4173c
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-08-02 14:14:41 +0200

    Use COMPUTED_JSON when computing SLASH_COMPUTED_JSON

--- src/main/java/com/iexec/common/utils/IexecFileHelper.java
+++ src/main/java/com/iexec/common/utils/IexecFileHelper.java
@@ -31,7 +31,7 @@ public class IexecFileHelper {
     public static final String SLASH_OUTPUT = File.separator + "output";
     public static final String SLASH_INPUT = File.separator + "input";    
     public static final String COMPUTED_JSON = "computed.json";
-    public static final String SLASH_COMPUTED_JSON = File.separator + "computed.json";
+    public static final String SLASH_COMPUTED_JSON = File.separator + COMPUTED_JSON;
 
     private IexecFileHelper() {
         throw new UnsupportedOperationException();
