6145e77370535e6756ee515adf582a146aaa551f
Maxence Cornaton <maxence.cornaton@iex.ec>
2022-09-09 11:36:21 +0200

    Introduce `ExpiringTaskMapFactory`

---
+++ src/main/java/com/iexec/common/utils/purge/ExpiringTaskMapFactory.java
@@ -0,0 +1,25 @@
+package com.iexec.common.utils.purge;
+
+import net.jodah.expiringmap.ExpiringMap;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * An expiring task map is defined as a map of {@link String} as keys
+ * and any type as keys,
+ * whereas the map can be purged after a given amount of time -
+ * max duration of any task, currently of 100 hours.
+ */
+public class ExpiringTaskMapFactory {
+    private ExpiringTaskMapFactory() {
+
+    }
+
+    public static <T> Map<String, T> getExpiringTaskMap() {
+        return ExpiringMap
+                .builder()
+                .expiration(100, TimeUnit.HOURS)
+                .build();
+    }
+}
