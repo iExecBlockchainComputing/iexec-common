60c7ccd0e441ba7e55601aae5722789601473c10
Maxence Cornaton <maxence.cornaton@iex.ec>
2022-09-14 11:32:53 +0200

    Remove `@Disabled` annotation on `DockerClientInstanceTests`

--- src/test/java/com/iexec/common/docker/client/DockerClientInstanceTests.java
+++ src/test/java/com/iexec/common/docker/client/DockerClientInstanceTests.java
@@ -53,7 +53,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-@Disabled
 @Tag("slow")
 class DockerClientInstanceTests {
 
