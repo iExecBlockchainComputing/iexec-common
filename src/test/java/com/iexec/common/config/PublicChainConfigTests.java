aea56325ff25342dd1a2a38b6724181e6ec0b934
Jeremy Bernard <jeremy.bernard@iex.ec>
2023-03-28 12:11:40 +0200

    Update `PublicChainConfigTests` class

--- src/test/java/com/iexec/common/config/PublicChainConfigTests.java
+++ src/test/java/com/iexec/common/config/PublicChainConfigTests.java
@@ -12,19 +12,20 @@ import java.time.Duration;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-public class PublicChainConfigTests {
+class PublicChainConfigTests {
 
     private final String errorMessage = "Java 8 date/time type `java.time.Duration` not supported by default";
 
     @Test
     void serializationShouldFailIfNoJavaTimeModule() {
         ObjectMapper mapper = new ObjectMapper();
-        PublicChainConfig config = new PublicChainConfig();
-        config.setBlockTime(Duration.ofSeconds(5L));
-        config.setChainId(1);
-        config.setChainNodeUrl("http://localhost:8545");
-        config.setIexecHubContractAddress("0xabcd");
-        config.setSidechain(false);
+        PublicChainConfig config = PublicChainConfig.builder()
+                .blockTime(Duration.ofSeconds(5L))
+                .chainId(1)
+                .chainNodeUrl("http://localhost:8545")
+                .iexecHubContractAddress("0xabcd")
+                .sidechain(false)
+                .build();
         assertThatThrownBy(() -> mapper.writeValueAsString(config))
                 .isInstanceOf(InvalidDefinitionException.class)
                 .hasMessageContaining(errorMessage);
@@ -45,12 +46,13 @@ public class PublicChainConfigTests {
         ObjectMapper mapper = JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .build();
-        PublicChainConfig refChainConfig = new PublicChainConfig();
-        refChainConfig.setBlockTime(Duration.ofSeconds(5L));
-        refChainConfig.setChainId(15);
-        refChainConfig.setChainNodeUrl("http://localhost:8545");
-        refChainConfig.setIexecHubContractAddress("0xabcd");
-        refChainConfig.setSidechain(true);
+        PublicChainConfig refChainConfig = PublicChainConfig.builder()
+                .blockTime(Duration.ofSeconds(5L))
+                .chainId(15)
+                .chainNodeUrl("http://localhost:8545")
+                .iexecHubContractAddress("0xabcd")
+                .sidechain(true)
+                .build();
         String jsonString = mapper.writeValueAsString(refChainConfig);
         PublicChainConfig chainConfig = mapper.readValue(jsonString, PublicChainConfig.class);
         assertThat(chainConfig).usingRecursiveComparison().isEqualTo(refChainConfig);
